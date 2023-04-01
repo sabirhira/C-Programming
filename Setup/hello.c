d67a5ca6f72502bbe6a44bdc71e2bbc685c28d81
sabirhira <sabhira101@gmail.com>
2023-04-01 17:06:36 +0600

    setup

---
+++ Setup/hello.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int x,y,sum;
+    scanf("%d %d",&x,&y);
+    sum=x+y;
+    printf("Hello! this is SabirHira from Bangladesh.\n\n");
+    printf("1st time| The sum of the vaiables is: %d\n",sum);
+    scanf("%d %d",&x,&y);
+    sum=x+y;
+    printf("2nd time| The sum of the vaiables is: %d\n",sum);
+    scanf("%d %d",&x,&y);
+    sum=x+y;
+    printf("3nd time| The sum of the vaiables is: %d\n",sum);
+    return 0;
+}
